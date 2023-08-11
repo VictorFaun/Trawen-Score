55cf0af2812807b433a7507e01b3a98f860f00fa
Victor Faundez <victor.faundez@impact.cl>
2023-08-11 08:11:29 -0400

    app base

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cl.thedifference.trawenscore',
+  appName: 'Trawen Score',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
